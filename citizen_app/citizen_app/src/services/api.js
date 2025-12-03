c5db6873482692006395ee8ac617355df78abb43
VelmuruganSuresh <velmurugan812415@gmail.com>
2025-12-03 09:26:44 +0530

    second-commit

--- citizen_app/citizen_app/src/services/api.js
+++ citizen_app/citizen_app/src/services/api.js
@@ -1,2 +1,3 @@
-export const BASE_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000";
+export const BASE_URL = process.env.REACT_APP_API_URL || "https://velmurugan001-civic-ai-backend.hf.space";
+
 export const API_URL = `${BASE_URL}/api`;
